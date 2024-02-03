3ef4e9c84456fa46331b8c447cb09bc209e1b85f
JyotiGhising1 <83853614+JyotiGhising1@users.noreply.github.com>
2024-02-03 00:57:53 -0500

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+const mondayWork = function (activity= 'go to the office'){
+    return `This Monday, I will ${activity}.`;
+};
+mondayWork();
+
+function wrapAdjective(symbol = "*"){
+    return function(adjective = "special"){
+        return `You are ${symbol}${adjective}${symbol}!`;
+    };
+}
+wrappedText(wrapAdjective(symbol)(adjective));
